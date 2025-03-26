0465f6ff473e519cf4c67bc9b1b587047cab45de
StephanyNM4 <stephanymatamoros4@gmail.com>
2025-03-26 15:06:14 -0600

    reportar usuario

--- src/Controllers/Trip.controller.ts
+++ src/Controllers/Trip.controller.ts
@@ -43,3 +43,44 @@ export const getTripDetails = async (req: Request, res: Response) => {
             });
         }
     };
+
+    export const reportTripUser = async (req: Request, res: Response) => {
+        try{
+            
+            const {
+                tripId,
+                reportStatusId,
+                reportedUserId,
+                reportingUserId,
+                reportSubject,
+                reportDescription,
+                reporterDate,
+                reportUrlImages
+            } = req.body;
+            
+            const data = await Trip.reportTripUser(
+                tripId,
+                reportStatusId,
+                reportedUserId,
+                reportingUserId,
+                reportSubject,
+                reportDescription,
+                reporterDate,
+                reportUrlImages
+                            );
+    
+            res.status(201).json({
+                data
+            })
+        } catch (error) {
+            const errorInfo = error && typeof error === 'object'
+                ? JSON.stringify(error, null, 2)
+                : error?.toString() || 'Unknown error';
+    
+            console.error('Error Information: ', errorInfo);
+            res.status(500).json({
+                message: 'Error Information: ', 
+                error: errorInfo
+            });
+        }
+    };
--- src/Routes/Trip.routes.ts
+++ src/Routes/Trip.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getTripDetails, cancelTripFromDriver } from '../Controllers/Trip.controller';
+import { getTripDetails, cancelTripFromDriver, reportTripUser } from '../Controllers/Trip.controller';
 
 const router = express.Router();
 
 
 router.get("/get/trips/:id", getTripDetails);
 router.put("/:id/cancel/driver", cancelTripFromDriver);
+router.post("/report", reportTripUser);
+
 
 
 export default router;
